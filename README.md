5d935b93c483fd63b7fb67994839d143564a17ff
Jesse N <jessenich@live.com>
2023-04-14 15:04:45 -0400

    added check for corresponding PDB and modifed flags accordingly

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Docker Project Template
+# Docker ilspycmd Image
+
+## Usage
+
+```bash
+bash ./build.sh
+bash ./run.sh < /path/to/binary > < /output/path >
+```
--- run.sh
+++ run.sh
@@ -2,20 +2,27 @@
 
 # shellcheck disable=SC2046,SC2086
 
-DLL=$1
-PDB=${1//.dll/.pdb} # | sed s/\.dll/\.pdb/g)
-OUT=$2
-docker run \
-  --volume "$DLL:/data/in/$(basename $DLL)" \
-  --volume "$PDB:/data/in/$(basename $PDB)" \
-  --volume "$OUT:/data/out" \
-  --rm \
-  jessenich91/ilspycmd:7.2.1 \
-  --outputdir "/data/out" \
-  --use-varnames-from-pdb \
-  --generate-pdb \
-  --project \
-  --no-dead-code \
-  --no-dead-stores \
-  --dump-package \
-  /data/in/$(basename $DLL)
+__main() {
+  DLL=$1
+  PDB=${1//.dll/.pdb} # | sed s/\.dll/\.pdb/g)
+  OUT=$2
+
+  if [[ -f $PDB ]]; then local -r USE_PDB=true; fi
+
+  docker run \
+    --volume "$DLL:/data/in/$(basename $DLL)" \
+    ${USE_PDB:+--volume $PDB:/data/in/$(basename $PDB)} \
+    --volume "$OUT:/data/out" \
+    --rm \
+    jessenich91/ilspycmd:7.2.1 \
+    --outputdir "/data/out" \
+    ${USE_PDB:+--use-varnames-from-pdb} \
+    ${USE_PDB:+--generate-pdb} \
+    --project \
+    --no-dead-code \
+    --no-dead-stores \
+    --dump-package \
+    /data/in/$(basename $DLL)
+}
+
+__main "$@"
